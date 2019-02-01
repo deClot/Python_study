951e7b4c51dd2ac511e4d9428fd8cbe1556e02ce
Nadezda Kashirina <refenement@mail.ru>
2019-02-01 20:48:50 +0700

    Add function to find matching in ancestry

--- Stepik/error_exception_delete_inheritance.py
+++ Stepik/error_exception_delete_inheritance.py
@@ -1,3 +1,14 @@
+def find_ancestry(excesption, parents, input_exception):
+    if len(parents) == 0:
+        return False
+    
+    if input_exception in parents:
+        return True
+    else:
+        for parent in parents:
+            find_ancestry(parent, exceptions[parent], input_exception)
+    
+
 n = input() # number of exceptions
 exceptions = {}
 
@@ -5,17 +16,27 @@ for _ in range(int(n)):
     string = input().split()
 
     if len(string) > 1:
-        exceptions[string[0]] = stirng[2:]
+        exceptions[string[0]] = string[2:]
 
     else:
-        exceptions[string[0]] = ()
+        exceptions[string[0]] = []
 
 m = input() # number of inputed exceptions
 input_exceptions = []
+out = []
 
 for _ in range(int(m)):
-    input_exceptions.append(input())
-  
-print (input_exceptions)
+    exception = input()
+    input_exceptions.append(exception)
+    parents = exceptions[exception]
 
+    for i in range(len(input_exceptions)-1):
+        flag = find_ancestry(exception, parents, input_exceptions[i])
+
+        if flag == False:
+            continue
+        else:
+            print (exception)
+            break
+    
     
