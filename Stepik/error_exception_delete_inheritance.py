c5eb921d9d599374760549f5a987dd82e1135a4b
Nadezda Kashirina <refenement@mail.ru>
2019-02-01 22:10:54 +0700

    Use function of stepic tutor

--- Stepik/error_exception_delete_inheritance.py
+++ Stepik/error_exception_delete_inheritance.py
@@ -1,51 +1,29 @@
 def is_ancestry(child, parent):
+    return child == parent or any(map(lambda p: is_ancestry(p, parent), exceptions[child]))
     
-
-'''
-def find_parent(parent, child):
-    if parent == child:
-        return True
-    if len(parents[child]) == 0:
-            return False
-        
-    for i in range(len(parents[child])):
-        if parents[child][i] == parent:
-            return True
-        else:
-            if find_parent(parent, parents[child][i]) is True:
-                return True
-            else:
-                continue
-    return False
-'''
-
 n = input() # number of exceptions
 exceptions = {}
 
 for _ in range(int(n)):
     string = input().split()
-
-    if len(string) > 1:
-        exceptions[string[0]] = string[2:]
-
-    else:
-        exceptions[string[0]] = []
+    exceptions[string[0]] = [] if len(string) == 1 else string[2:]
 
 m = input() # number of inputed exceptions
 input_exceptions = []
 out = []
 
 for _ in range(int(m)):
-    exception = input()
-    input_exceptions.append(exception)
+    child = input()
+    input_exceptions.append(child)
 
     for i in range(len(input_exceptions)-1):
-        flag = is_ancestry(exception, input_exceptions[i])
+        parent = input_exceptions[i]
+        flag = is_ancestry(child, parent)
 
         if flag == False:
             continue
         else:
-            print (exception)
+            print(child)
             break
     
-    
+   
