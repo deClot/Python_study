8058baa4b747b3e4f6e4ded685b08d9e3f8f5842
Nadezda Kashirina <refenement@mail.ru>
2019-02-01 13:48:08 +0700

    Create inputs for all exceptionS

---
+++ Stepik/error_exception_delete_inheritance.py
@@ -0,0 +1,21 @@
+n = input() # number of exceptions
+exceptions = {}
+
+for _ in range(int(n)):
+    string = input().split()
+
+    if len(string) > 1:
+        exceptions[string[0]] = stirng[2:]
+
+    else:
+        exceptions[string[0]] = ()
+
+m = input() # number of inputed exceptions
+input_exceptions = []
+
+for _ in range(int(m)):
+    input_exceptions.append(input())
+  
+print (input_exceptions)
+
+    
