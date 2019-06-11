21db5837b225b16ba7453b042ceaaa238635925e
declot <refenement@mail.ru>
2019-06-11 16:46:09 +0700

    Opportunistic Encryption with starttls

--- sending_emails.py
+++ sending_emails.py
@@ -1,14 +1,26 @@
 import smtplib, ssl
 
 smtp_server = 'smtp.gmail.com'  # for gmail
-port = 465 # for  ssl
+port = 587 # for ssl
 
 sender = 'refenement2@gmail.com'
 password = input('Enter your password here: ')
 
-#Encryprion context
+# Encryprion context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+# Creating unencrypted connection and attempt upgrade it
+try:
+    server = smtplib.SMTP(smtp_server, port) #make a server object
+    server.ehlo() # indentify yourself
+    server.starttls(context=context) #upgrade connection
+    server.ehlo()
     server.login(sender, password)
+
     print('It worked!')
+
+except Exception as e:
+    print(e)
+finally:
+    server.quit() #close connection to the server
+    
