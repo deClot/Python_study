c00b40eeb1dadbd8dc0abae5084a7ce8b7618488
declot <refenement@mail.ru>
2019-06-14 14:27:07 +0700

    Create table with some cities

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,22 +1,27 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 def index(request):
     appid = 'a8a709791f27a328cc9693ae6f0ab678'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'London'
+    cities = City.objects.all()
+    all_cities = []
 
-    res = requests.get(url.format(city)).json()
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        print(res)
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]['icon']
+        }
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]['icon']
-    }
+        all_cities.append(city_info)
 
-    context = {'info': city_info}
+    context = {'all_info': all_cities}
 
     return render(request, 'weather/index.html', context)  #by default, it starts to looking for in /templates
 
