9d0d9d03b61ce53d8c4ecdaf94112b5c88f937a4
declot <refenement@mail.ru>
2019-06-14 13:49:34 +0700

    Add weather api

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,22 @@
+import requests
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'weather/index.html')  #by default, it starts to looking for in /templates 
+    appid = 'a8a709791f27a328cc9693ae6f0ab678'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    city = 'London'
+
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]['icon']
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html', context)  #by default, it starts to looking for in /templates
+
