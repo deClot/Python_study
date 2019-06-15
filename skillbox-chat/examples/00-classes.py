e58e2cda883b244a84d9fd6414533a4f6c1223e7
Nadezda Kashirina <refenement@mail.ru>
2019-06-15 13:37:14 +0700

    Work with properties of classes

---
+++ skillbox-chat/examples/00-classes.py
@@ -0,0 +1,29 @@
+"""
+Основы ООП - конструктор, наследование, перегрузка, полиморфизм, инкапсуляция
+"""
+
+
+class Human:
+    first_name: str
+    last_name: str
+
+    def __init__(self, first_name, last_name='Undefined'):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'Human: {self.first_name} {self.last_name}'
+
+
+class User(Human):
+    __passport: str = "1234 123123"
+
+    def hello(self):
+        print(f'Hello, {self.first_name}!')
+
+
+john = User('John', 'Doe')
+john.hello()
+print(john)
+
+print(john._User__passport)
