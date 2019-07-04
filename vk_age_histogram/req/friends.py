70bda392184895be21cd9ceab2264a078709a341
Nadezda Kashirina <refenement@mail.ru>
2019-07-04 15:06:41 +0700

    Add sort friends

--- vk_age_histogram/req/friends.py
+++ vk_age_histogram/req/friends.py
@@ -9,11 +9,27 @@ def calc_age(uid):
     print(user_id)
 
     url  = 'https://api.vk.com/method/{}.get?v=5.71&access_token={}&user_id={}&fields=bdate'
-    response = requests.get(url.format('friends', token, user_id))
-    print(response.text)
- 
-#params = {'user_id': us_id, 'fields': 'bdate'}
+    response = requests.get(url.format('friends', token, user_id)).json()
+    #number_friends = response['response']['count']
+    friends = response['response']['items']
+
+    #print(number_friends)
+    data_hist = {}
+    for friend in friends:
+        #print(friend)
+        try:
+            age = 2019 - int(friend['bdate'].split('.')[2])
+            data_hist[age] = data_hist.get(age,0) + 1
+        except:
+            continue
+
+    data_hist = data_hist.items()
+    s = sorted(data_hist, key=lambda x: x[0])
+    return sorted(s, key=lambda x: x[1], reverse=True)
+        
 
 if __name__ == '__main__':
     res = calc_age('reigning')
     print(res)
+
+
